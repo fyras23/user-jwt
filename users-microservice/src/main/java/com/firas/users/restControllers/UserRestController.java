e18dc046283f7dc7103c3f29d12e33de272a77d4
fyras23 <115085893+fyras23@users.noreply.github.com>
2023-05-18 23:39:58 +0100

    Add files via upload

---
+++ users-microservice/src/main/java/com/firas/users/restControllers/UserRestController.java
@@ -0,0 +1,21 @@
+package com.firas.users.restControllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firas.users.entities.User;
+import com.firas.users.service.UserService;
+@RestController
+@CrossOrigin(origins = "*")
+public class UserRestController {
+@Autowired
+UserService userService;
+@RequestMapping(path = "all", method = RequestMethod.GET)
+public List<User> getAllUsers() {
+return userService.findAllUsers();
+}
+}
